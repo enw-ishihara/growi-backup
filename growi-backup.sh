288a1b057190d62bea4c7d1126d881d94d0a36a6
Hiro <hiroyuki1126@gmail.com>
2022-05-04 23:08:13 +0900

    バックアップ・リストア処理を関数化。

--- growi-backup.sh
+++ growi-backup.sh
@@ -4,7 +4,24 @@ COMPOSE_FILE=docker-compose.yml
 
 readonly DOCKER_DIR='/var/opt/github/weseek/growi'
 readonly BACKUP_DIR='/var/opt/backup'
-readonly BACKUP_GEN=2
+readonly BACKUP_GEN=7
+
+function error() {
+    echo "ERROR: $(basename $0): $@" 1>&2
+}
+
+function abort() {
+    error $@
+    exit 1
+}
+
+function backup() {
+    local container_name=$1
+    local file_name=$2
+    local target_dir=$3
+
+    docker run --rm --volumes-from ${container_name} -v ${BACKUP_DIR}:/var/opt/backup busybox tar cf /var/opt/backup/${file_name} ${target_dir}
+}
 
 function remove_old_backup() {
     local target=$1
@@ -13,22 +30,19 @@ function remove_old_backup() {
 }
 
 cd ${DOCKER_DIR}
-docker-compose stop
+docker-compose stop || abort 'docker-compose stop faild.'
 
-CONTAINER_NAME='growi_app_1'
-docker run --rm --volumes-from ${CONTAINER_NAME} -v ${BACKUP_DIR}:/var/opt/backup busybox tar cf /var/opt/backup/${CONTAINER_NAME}-growi_data-$(date '+%Y%m%d-%H%M%S').tar /data
-remove_old_backup "${CONTAINER_NAME}-growi_data-*.tar"
+backup 'growi-app-1' "app_data_$(date '+%Y%m%d-%H%M%S').tar" '/data'
+backup 'growi-mongo-1' "mongo_configdb_$(date '+%Y%m%d-%H%M%S').tar" '/data/configdb'
+backup 'growi-mongo-1' "mongo_db_$(date '+%Y%m%d-%H%M%S').tar" '/data/db'
+backup 'growi-elasticsearch-1' "elasticsearch_data_$(date '+%Y%m%d-%H%M%S').tar" '/usr/share/elasticsearch/data'
 
-CONTAINER_NAME='growi_mongo_1'
-docker run --rm --volumes-from ${CONTAINER_NAME} -v ${BACKUP_DIR}:/var/opt/backup busybox tar cf /var/opt/backup/${CONTAINER_NAME}-mongo_configdb-$(date '+%Y%m%d-%H%M%S').tar /data/configdb
-docker run --rm --volumes-from ${CONTAINER_NAME} -v ${BACKUP_DIR}:/var/opt/backup busybox tar cf /var/opt/backup/${CONTAINER_NAME}-mongo_db-$(date '+%Y%m%d-%H%M%S').tar /data/db
-remove_old_backup "${CONTAINER_NAME}-mongo_configdb-*.tar"
-remove_old_backup "${CONTAINER_NAME}-mongo_db-*.tar"
+cd ${DOCKER_DIR}
+docker-compose start || abort 'docker-compose start faild.'
 
-CONTAINER_NAME='growi_elasticsearch_1'
-docker run --rm --volumes-from ${CONTAINER_NAME} -v ${BACKUP_DIR}:/var/opt/backup busybox tar cf /var/opt/backup/${CONTAINER_NAME}-es_data-$(date '+%Y%m%d-%H%M%S').tar /usr/share/elasticsearch/data
-remove_old_backup "${CONTAINER_NAME}-es_data-*.tar"
-
-docker-compose start
+remove_old_backup 'app_data_*.tar'
+remove_old_backup 'mongo_configdb_*.tar'
+remove_old_backup 'mongo_db_*.tar'
+remove_old_backup 'elasticsearch_data_*.tar'
 
 exit 0
--- growi-restore.sh
+++ growi-restore.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-# リストア手順
-# 1. コンテナとデータボリュームを削除する
-#   docker-compose down -v
-# 2. コンテナとデータボリュームを作成する
-#   docker-compose up --no-start
-# 3. データのリストアを実行する
-#   growi-restore.sh growi_app_1 /var/opt/backup/[BACKUP_FILE]
-#   growi-restore.sh growi_mongo_1 /var/opt/backup/[BACKUP_FILE]
-#   growi-restore.sh growi_mongo_1 /var/opt/backup/[BACKUP_FILE]
-#   growi-restore.sh growi_elasticsearch_1 /var/opt/backup/[BACKUP_FILE]
-# 4. コンテナを起動する
-#   docker-compose start
+COMPOSE_FILE=docker-compose.yml
 
+readonly DOCKER_DIR='/var/opt/github/weseek/growi'
 readonly BACKUP_DIR='/var/opt/backup'
 
 function error() {
@@ -24,10 +14,31 @@ function abort() {
     exit 1
 }
 
-[ $# -eq 2 ] || abort "Usage: $0 [CONTAINER_NAME] [BACKUP_FILE]"
-readonly CONTAINER_NAME=$1
-readonly BACKUP_FILE=$2
+function restore() {
+    local container_name=$1
+    local file_name=$2
 
-docker run --rm --volumes-from ${CONTAINER_NAME} -v ${BACKUP_DIR}:/var/opt/backup busybox tar xf ${BACKUP_FILE}
+    echo -e "\n* Backup file list [container: ${container_name}][${file_name}]"
+    cd ${BACKUP_DIR}
+    ls -ltr ${file_name}
+    echo '--------------------------------------------------'
+    echo -n 'Input backup filename: '
+    read backup_file || abort 'read faild.'
+    [ -f ${backup_file} ] || abort "file ${backup_file} not found."
+    docker run --rm --volumes-from ${container_name} -v ${BACKUP_DIR}:/var/opt/backup busybox tar xf /var/opt/backup/${backup_file}
+    echo 'Resotore done.'
+}
+
+cd ${DOCKER_DIR}
+docker-compose down -v || abort 'docker-compose down -v faild.'
+docker-compose up --no-start || abort 'docker-compose up --no-start faild.'
+
+restore 'growi-app-1' 'app_data_????????-??????.tar'
+restore 'growi-mongo-1' 'mongo_configdb_????????-??????.tar'
+restore 'growi-mongo-1' 'mongo_db_????????-??????.tar'
+restore 'growi-elasticsearch-1' 'elasticsearch_data_????????-??????.tar'
+
+cd ${DOCKER_DIR}
+docker-compose start || abort 'docker-compose start faild.'
 
 exit 0
